19c0ad46a2aed6b628e541006e6aa6763cca91e8
Gytis Trikleris <gytis@redhat.com>
2019-02-01 00:21:22 +0100

    Vert.x mail sample application

---
+++ samples/mail-sample/src/main/java/me/snowdrop/vertx/sample/mail/SampleApplication.java
@@ -0,0 +1,34 @@
+package me.snowdrop.vertx.sample.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> mailRouter(MailHandler mailHandler) {
+        return route()
+                .POST("/mail", accept(APPLICATION_FORM_URLENCODED), mailHandler::send)
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> staticResourceRouter() {
+        return resources("/**", new ClassPathResource("static/"));
+    }
+
+}
