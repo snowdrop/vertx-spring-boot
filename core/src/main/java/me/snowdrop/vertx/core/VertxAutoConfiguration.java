c6f041340c70e91cec668728ac37389df682e3e7
Gytis Trikleris <gytis@redhat.com>
2019-02-07 14:29:57 +0100

    Vertx core properties

--- core/src/main/java/me/snowdrop/vertx/core/VertxAutoConfiguration.java
+++ core/src/main/java/me/snowdrop/vertx/core/VertxAutoConfiguration.java
@@ -2,16 +2,18 @@ package me.snowdrop.vertx.core;
 
 import io.vertx.core.Vertx;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnClass(Vertx.class)
+@EnableConfigurationProperties(VertxProperties.class)
 public class VertxAutoConfiguration {
 
     @Bean(destroyMethod = "") // Leave destruction to VertxDisposingBean
-    public Vertx vertx() {
-        return Vertx.vertx();
+    public Vertx vertx(VertxProperties properties) {
+        return Vertx.vertx(properties.toVertxOptions());
     }
 
     @Bean // TODO find a better way to dispose a Vertx instance asynchronously
--- core/src/test/java/me/snowdrop/vertx/core/VertxAutoConfigurationTest.java
+++ core/src/test/java/me/snowdrop/vertx/core/VertxAutoConfigurationTest.java
@@ -6,10 +6,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @Import(VertxAutoConfiguration.class)
 public class VertxAutoConfigurationTest {
 
@@ -20,6 +21,5 @@ public class VertxAutoConfigurationTest {
     public void shouldInjectVertxInstance() {
         assertThat(vertx).isNotNull();
     }
-
 
 }
