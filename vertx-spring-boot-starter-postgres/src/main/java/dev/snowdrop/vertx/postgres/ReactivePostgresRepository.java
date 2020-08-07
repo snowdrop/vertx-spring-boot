718f661659fd8841d438b32f7e294374ebdc86a0
Gytis Trikleris <gytis@redhat.com>
2020-08-07 12:31:41 +0300

    Remove postgres module

--- vertx-spring-boot-starter-postgres/src/main/java/dev/snowdrop/vertx/postgres/ReactivePostgresRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.snowdrop.vertx.postgres;
-
-import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-@NoRepositoryBean
-public interface ReactivePostgresRepository<T, ID> extends ReactiveCrudRepository<T, ID> {
-
-}
--- vertx-spring-boot-starter-postgres/src/test/java/dev/snowdrop/vertx/postgres/it/TestReactivePostgresRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.snowdrop.vertx.postgres.it;
-
-import dev.snowdrop.vertx.postgres.ReactivePostgresRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestReactivePostgresRepository extends ReactivePostgresRepository<Person, String> {
-}
