92af460cc5c801cfa5fb267d5cbf95468dc7342b
Gytis Trikleris <gytis@redhat.com>
2020-09-25 16:15:10 +0200

    feat: Use Mutiny instead of Axle for AMQP starter

--- vertx-spring-boot-starter-amqp/src/main/java/dev/snowdrop/vertx/amqp/MessageConverter.java
+++ vertx-spring-boot-starter-amqp/src/main/java/dev/snowdrop/vertx/amqp/MessageConverter.java
@@ -2,11 +2,11 @@ package dev.snowdrop.vertx.amqp;
 
 class MessageConverter {
 
-    io.vertx.axle.amqp.AmqpMessage toAxleMessage(dev.snowdrop.vertx.amqp.AmqpMessage snowdropMessage) {
-        return new io.vertx.axle.amqp.AmqpMessage(snowdropMessage.toVertxAmqpMessage());
+    io.vertx.mutiny.amqp.AmqpMessage toMutinyMessage(dev.snowdrop.vertx.amqp.AmqpMessage snowdropMessage) {
+        return new io.vertx.mutiny.amqp.AmqpMessage(snowdropMessage.toVertxAmqpMessage());
     }
 
-    dev.snowdrop.vertx.amqp.AmqpMessage toSnowdropMessage(io.vertx.axle.amqp.AmqpMessage axleMessage) {
-        return new dev.snowdrop.vertx.amqp.SnowdropAmqpMessage(axleMessage.getDelegate());
+    dev.snowdrop.vertx.amqp.AmqpMessage toSnowdropMessage(io.vertx.mutiny.amqp.AmqpMessage mutinyMessage) {
+        return new dev.snowdrop.vertx.amqp.SnowdropAmqpMessage(mutinyMessage.getDelegate());
     }
 }
--- vertx-spring-boot-starter-amqp/src/main/java/dev/snowdrop/vertx/amqp/SnowdropAmqpClient.java
+++ vertx-spring-boot-starter-amqp/src/main/java/dev/snowdrop/vertx/amqp/SnowdropAmqpClient.java
@@ -2,53 +2,66 @@ package dev.snowdrop.vertx.amqp;
 
 import java.time.Duration;
 
+import io.smallrye.mutiny.converters.uni.UniReactorConverters;
 import org.springframework.beans.factory.DisposableBean;
 import reactor.core.publisher.Mono;
 
 class SnowdropAmqpClient implements AmqpClient, DisposableBean {
 
-    private final io.vertx.axle.amqp.AmqpClient delegate;
+    private final io.vertx.mutiny.amqp.AmqpClient delegate;
 
     private final MessageConverter messageConverter;
 
-    SnowdropAmqpClient(io.vertx.axle.amqp.AmqpClient delegate, MessageConverter messageConverter) {
+    SnowdropAmqpClient(io.vertx.mutiny.amqp.AmqpClient delegate, MessageConverter messageConverter) {
         this.delegate = delegate;
         this.messageConverter = messageConverter;
     }
 
     @Override
     public Mono<AmqpConnection> connect() {
-        return Mono.fromCompletionStage(delegate::connect)
+        return delegate.connect()
+            .convert()
+            .with(UniReactorConverters.toMono())
             .map(delegateConnection -> new SnowdropAmqpConnection(delegateConnection, messageConverter));
     }
 
     @Override
     public Mono<AmqpSender> createSender(String address) {
-        return Mono.fromCompletionStage(() -> delegate.createSender(address))
+        return delegate.createSender(address)
+            .convert()
+            .with(UniReactorConverters.toMono())
             .map(delegateSender -> new SnowdropAmqpSender(delegateSender, messageConverter));
     }
 
     @Override
     public Mono<AmqpSender> createSender(String address, AmqpSenderOptions options) {
-        return Mono.fromCompletionStage(() -> delegate.createSender(address, options.toVertxAmqpSenderOptions()))
+        return delegate.createSender(address, options.toVertxAmqpSenderOptions())
+            .convert()
+            .with(UniReactorConverters.toMono())
             .map(delegateSender -> new SnowdropAmqpSender(delegateSender, messageConverter));
     }
 
     @Override
     public Mono<AmqpReceiver> createReceiver(String address) {
-        return Mono.fromCompletionStage(() -> delegate.createReceiver(address))
+        return delegate.createReceiver(address)
+            .convert()
+            .with(UniReactorConverters.toMono())
             .map(delegateReceiver -> new SnowdropAmqpReceiver(delegateReceiver, messageConverter));
     }
 
     @Override
     public Mono<AmqpReceiver> createReceiver(String address, AmqpReceiverOptions options) {
-        return Mono.fromCompletionStage(() -> delegate.createReceiver(address, options.toVertxAmqpReceiverOptions()))
+        return delegate.createReceiver(address, options.toVertxAmqpReceiverOptions())
+            .convert()
+            .with(UniReactorConverters.toMono())
             .map(delegateReceiver -> new SnowdropAmqpReceiver(delegateReceiver, messageConverter));
     }
 
     @Override
     public Mono<Void> close() {
-        return Mono.fromCompletionStage(delegate::close);
+        return delegate.close()
+            .convert()
+            .with(UniReactorConverters.toMono());
     }
 
     @Override
